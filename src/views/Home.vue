5c8c27e92cf1b44fdc3d27efdb046d666aba228b
Kenneth <gilmorekenneth@gmail.com>
2021-03-02 10:55:50 -0500

    Init commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h3>hello world</h3>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  // components: {
+  //   HelloWorld,
+  // },
+  setup() {
+    fetch("https://api.github.com/organizations").then((res) =>
+      res.json().then((data) => console.log(data))
+    );
+  },
+};
 </script>
